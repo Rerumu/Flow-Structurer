0283534e58187c6c79c2d30c8af48b01788a21e0
Rerumu <25379555+Rerumu@users.noreply.github.com>
2024-02-04 03:41:13 -0500

    Add titular academic paper reference

--- src/restructurer/mod.rs
+++ src/restructurer/mod.rs
@@ -1,1 +1,8 @@
+// Resources:
+// "Perfect Reconstructability of Control Flow from Demand Dependence Graphs",
+//     by Helge Bahmann, Google Zurich, Nico Reissmann, Magnus Jahre, and Jan Christian Meyer
+
 mod depth_first_searcher;
+
+pub mod branch;
+pub mod repeat;
