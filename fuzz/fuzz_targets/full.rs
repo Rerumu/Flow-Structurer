c29266475072a048939f8f1ff0049e00df9da962
Rerumu <25379555+Rerumu@users.noreply.github.com>
2024-02-04 03:51:28 -0500

    Add full fuzz target

---
+++ fuzz/fuzz_targets/full.rs
@@ -0,0 +1,15 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use list::List;
+use perfect_reconstructibility::restructurer::{branch, repeat};
+
+mod list;
+
+fuzz_target!(|list: List| {
+	let mut list = list;
+	let mut set = list.ids();
+
+	repeat::bulk::Bulk::new().restructure(&mut list, &mut set);
+	branch::bulk::Bulk::new().restructure(&mut list, &mut set, 0);
+});
