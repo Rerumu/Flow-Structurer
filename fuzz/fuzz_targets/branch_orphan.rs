1a0780f637b628bdb0f01a2b0852b44b04decff5
Rerumu <25379555+Rerumu@users.noreply.github.com>
2024-08-03 17:05:33 -0400

    Add orphan fuzz target

---
+++ fuzz/fuzz_targets/branch_orphan.rs
@@ -0,0 +1,27 @@
+#![no_main]
+
+use flow_structurer::{
+	branch::Branch,
+	nodes::{Flag, Nodes, Successors},
+};
+use libfuzzer_sys::fuzz_target;
+
+use crate::sample::arbitrary::DirectedAcyclicGraph;
+
+mod sample;
+
+fuzz_target!(|built: DirectedAcyclicGraph| {
+	let mut list = built.into_inner();
+	let mut set = (0..list.len()).collect();
+
+	Branch::new().run(&mut list, &mut set, 0);
+
+	let has_orphans = set.ascending().any(|id| {
+		list.has_assignment(id, Flag::A)
+			&& list
+				.successors(id)
+				.any(|id| list.has_assignment(id, Flag::A))
+	});
+
+	assert!(!has_orphans, "`Branch` left orphans");
+});
